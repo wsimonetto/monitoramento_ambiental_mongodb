c477bdaf3ad96244745504b1cc0eebe7829aae53
Wagner Machado <wagnersimonetto@gmail.com>
2024-09-18 21:14:57 -0300

    Update Azure Variáveis

--- Data/MongoDBContext.cs
+++ Data/MongoDBContext.cs
@@ -1,39 +1,4 @@
-﻿//using Microsoft.Extensions.Options;
-//using MongoDB.Driver;
-//using monitoramento_ambiental_mongodb.Models;
-
-//namespace monitoramento_ambiental_mongodb.Data
-//{
-//    public class MongoDBContext
-//    {
-//        private readonly IMongoDatabase _database;
-//        private readonly DataBaseSettings _settings;
-
-//        public MongoDBContext(IOptions<DataBaseSettings> options)
-//        {
-//            _settings = options.Value;
-//            var client = new MongoClient(_settings.ConnectionURI);
-//            _database = client.GetDatabase(_settings.DatabaseName);
-//        }
-
-//        public IMongoCollection<SensorModel> Sensores =>
-//            _database.GetCollection<SensorModel>(_settings.SensorCollectionName);
-
-//        public IMongoCollection<LeituraModel> Leituras =>
-//            _database.GetCollection<LeituraModel>(_settings.LeituraCollectionName);
-
-//        public IMongoCollection<PrevisaoChuvaModel> PrevisoesChuva =>
-//            _database.GetCollection<PrevisaoChuvaModel>(_settings.PrevisaoChuvaCollectionName);
-
-//        public IMongoCollection<ControleIrrigacaoModel> ControleIrrigacoes =>
-//            _database.GetCollection<ControleIrrigacaoModel>(_settings.ControleIrrigacaoCollectionName);
-
-//        public IMongoCollection<AlertaModel> Alertas =>
-//            _database.GetCollection<AlertaModel>(_settings.AlertaCollectionName);
-//    }
-//}
-
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using monitoramento_ambiental_mongodb.Models;
 
@@ -47,12 +12,10 @@ namespace monitoramento_ambiental_mongodb.Data
         public MongoDBContext(IOptions<DataBaseSettings> options)
         {
             _settings = options.Value ?? throw new ArgumentNullException(nameof(options));
-<<<<<<< HEAD
             if (string.IsNullOrEmpty(_settings.ConnectionURI))
                 throw new ArgumentException("ConnectionURI cannot be null or empty", nameof(_settings.ConnectionURI));
             if (string.IsNullOrEmpty(_settings.DatabaseName))
                 throw new ArgumentException("DatabaseName cannot be null or empty", nameof(_settings.DatabaseName));
-=======
 
             if (string.IsNullOrEmpty(_settings.ConnectionURI))
             {
@@ -63,7 +26,6 @@ namespace monitoramento_ambiental_mongodb.Data
             {
                 throw new InvalidOperationException("DatabaseName não está configurado.");
             }
->>>>>>> 8bdfd3e (Update Variáveis no Azure)
 
             var client = new MongoClient(_settings.ConnectionURI);
             _database = client.GetDatabase(_settings.DatabaseName);
@@ -85,10 +47,3 @@ namespace monitoramento_ambiental_mongodb.Data
             _database.GetCollection<AlertaModel>(_settings.AlertaCollectionName);
     }
 }
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 8bdfd3e (Update Variáveis no Azure)
--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 # Base image for runtime
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
-=======
 #See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
 USER app
->>>>>>> 8bdfd3e (Update Variáveis no Azure)
 WORKDIR /app
 EXPOSE 8080
 EXPOSE 8081
 
-<<<<<<< HEAD
 # Build image
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
@@ -23,7 +19,6 @@ WORKDIR "/src/."
 RUN dotnet build "./monitoramento_ambiental_mongodb.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
 # Publish image
-=======
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
@@ -33,21 +28,17 @@ COPY . .
 WORKDIR "/src/."
 RUN dotnet build "./monitoramento_ambiental_mongodb.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
->>>>>>> 8bdfd3e (Update Variáveis no Azure)
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
 RUN dotnet publish "./monitoramento_ambiental_mongodb.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
 
-<<<<<<< HEAD
 # Final image
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
 
 ENTRYPOINT ["dotnet", "monitoramento_ambiental_mongodb.dll"]
-=======
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
 ENTRYPOINT ["dotnet", "monitoramento_ambiental_mongodb.dll"]
->>>>>>> 8bdfd3e (Update Variáveis no Azure)
